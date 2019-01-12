7c947b380a1bbabc3b32e43cf9ace54e7ed1c915
Zachy Bazov <zahibazov@gmail.com>
2019-01-12 17:21:08 +0200

    modifications

---
+++ app/src/main/java/com/corespark/pccompiler/activity/ss.kt
@@ -0,0 +1,10 @@
+package com.corespark.pccompiler.activity
+
+
+/**
+ * @author Zachy Bazov.
+ * @since 12/01/2019.
+ * CoreSpark Ltd.
+ * PCCompiler.
+ * All Rights Reserved. Copyright (c) 2018.
+ */
